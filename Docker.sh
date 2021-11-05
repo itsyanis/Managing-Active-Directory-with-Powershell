e67da97054e85f650f8408cabe2d19c383761505
Yanis <93043965+itsyanis@users.noreply.github.com>
2021-11-05 14:43:21 +0100

    stop container & delete image

--- Docker.sh
+++ Docker.sh
@@ -6,7 +6,9 @@ function Menu_Docker {
    echo "3- Répertorier tous les conteneurs docker exécutés "
    echo "4- Executer une image docker"
    echo "5- Démarrer un conteneur docker"
-   echo "6- Supprimer un conteneur docker"
+   echo "6- Arreter un conteneur docker"
+   echo "7- Supprimer un conteneur docker"
+   echo "8- Supprimer une image docker"
 }
 
 
@@ -45,18 +47,22 @@ function Docker {
 
 
     6)
-      echo -n "Entrer l'ID du conteneur à démarrer : "
+      echo -n "Entrer l'ID du conteneur à arreter : "
       read Image
       docker run -it -d $ID
       ;;
-
-
+      
     7)
       echo -n "Entrer l'ID du conteneur à supprimer : "
       read ID
-      docker rm $ID
+      docker stop $ID
       ;;
-
+      
+     8)
+      echo -n "Entrer l'ID de l'image à supprimer : "
+      read ID
+      docker rmi $ID
+      ;;
 
   esac
 
